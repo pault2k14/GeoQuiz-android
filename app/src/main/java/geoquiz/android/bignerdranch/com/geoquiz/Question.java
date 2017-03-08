37f73d7cc42e2ace91184d02dc5b1a23a10cbc0b
Paul Thompson <paul.thompson@nwea.org>
2017-03-07 17:29:22 -0800

    Added some classes

---
+++ app/src/main/java/geoquiz/android/bignerdranch/com/geoquiz/Question.java
@@ -0,0 +1,16 @@
+package geoquiz.android.bignerdranch.com.geoquiz;
+
+/**
+ * Created by Paul.Thompson on 3/7/2017.
+ */
+
+public class Question {
+
+    private int mTestResId;
+    private boolean mAnswerTrue;
+
+    public Question(int mTestResId, boolean mAnswerTrue) {
+
+    }
+
+}
